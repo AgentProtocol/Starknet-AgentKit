e308e9ee7e787f844934fc09dd963ee2d8c51984
gasperpre <gasper.pregelj98@gmail.com>
2024-11-07 23:30:29 +0700

    constants

--- langchain-agent/constants.ts
+++ langchain-agent/constants.ts
@@ -2,8 +2,6 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 export const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
-// export const STARKNET_ACCOUNT_ADDRESS = process.env.STARKNET_ACCOUNT_ADDRESS;
-// export const STARKNET_PRIVATE_KEY = process.env.STARKNET_PRIVATE_KEY;
-export const STARKNET_ACCOUNT_ADDRESS = undefined;
-export const STARKNET_PRIVATE_KEY = undefined;
+export const STARKNET_ACCOUNT_ADDRESS = process.env.STARKNET_ACCOUNT_ADDRESS;
+export const STARKNET_PRIVATE_KEY = process.env.STARKNET_PRIVATE_KEY;
 export const RPC_URL = process.env.RPC_URL;
